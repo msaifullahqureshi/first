fe79c0ffa369f90996923df162fcccd1c2e99b78
msaifullahqureshi <msaifullahqureshi8@gmail.com>
2025-12-06 12:35:23 +0500

    Update script.js for correct images folder name

--- script.js
+++ script.js
@@ -31,25 +31,25 @@ async function checkWeather(city) {
         document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + " km/h"
 
         if (data.weather[0].main == "Clouds") {
-            weatherIcon.src = "images/clouds.png"
+            weatherIcon.src = "Images/clouds.png"
         }
         else if (data.weather[0].main == "Clear") {
-            weatherIcon.src = "images/clear.png"
+            weatherIcon.src = "Images/clear.png"
         }
         else if (data.weather[0].main == "Rain") {
-            weatherIcon.src = "images/rain.png"
+            weatherIcon.src = "Images/rain.png"
         }
         else if (data.weather[0].main == "Drizzle") {
-            weatherIcon.src = "images/drizzle.png"
+            weatherIcon.src = "Images/drizzle.png"
         }
         else if (data.weather[0].main == "Mist") {
-            weatherIcon.src = "images/mist.png"
+            weatherIcon.src = "Images/mist.png"
         }
         else if (data.weather[0].main == "Haze") {
-            weatherIcon.src = "images/haze.png"
+            weatherIcon.src = "Images/haze.png"
         }
         else if (data.weather[0].main == "Smoke") {
-            weatherIcon.src = "images/haze.png"
+            weatherIcon.src = "Images/haze.png"
         }
 
         document.querySelector(".errors .propername").style.display = "none"
